34b656984c8fefccbe8112ec585df2badb9afca2
QuentGrd <quentin.gerard2@gmail.com>
2019-11-16 12:59:51 +0100

    Reduce to 4 button + center

--- lib/components/modeButtonList.dart
+++ lib/components/modeButtonList.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 
 class ModeButtonList extends StatefulWidget{
 
+  final List<Widget> buttons = initButtonList(4);
+
+  static List<Widget> initButtonList(int number) {
+     List<Widget> buttons = [];
+    for(int i=0; i<number; i++){
+      buttons.add(new ModeButton("mode$i", null));
+    }
+    return buttons;
+  }
   ModeButtonList({Key key}) : super(key: key);
 
   @override
@@ -13,18 +22,16 @@ class _ModeButtonListState extends State<ModeButtonList>{
   
   @override
   Widget build(BuildContext buildContext){
-    return ListView(
-      padding: EdgeInsets.only(top: 15.0, left: 15.0),
-      children: _getModeButtons(),
-      scrollDirection: Axis.horizontal,
+    return Container(
+        padding: EdgeInsets.only(top: 15.0, left: 15.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _getModeButtons(),
+        ),
     );
   }
 
   List<Widget> _getModeButtons(){
-    List<Widget> buttons = [];
-    for(int i=0; i<20; i++){
-      buttons.add(new ModeButton("mode$i", null));
-    }
-    return buttons;
+   return widget.buttons;
   }
 }
