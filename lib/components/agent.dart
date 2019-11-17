57e4f5478904b9add65a953d011120e68d396b99
QuentGrd <quentin.gerard2@gmail.com>
2019-11-17 10:07:08 +0100

    Add MAQTT connexion

--- lib/components/agent.dart
+++ lib/components/agent.dart
@@ -1,8 +1,11 @@
+import 'package:mqtt_client/mqtt_client.dart';
+
 class Agent{
   var position;
   var objective;
   var vehicle;
   var path;
+  final MqttClient client = MqttClient('mr1dns3dpz5mjj.messaging.solace.cloud', '');
 
   Agent(this.position, this.objective, this.vehicle, this.path);
 
@@ -13,4 +16,22 @@ class Agent{
   void setVehicle(var vehicle){this.vehicle = vehicle;}
 
   void setPath(var path){this.path = path;}
+
+  Future<int> run() async{
+    client.onConnected = onConnected;
+    try {
+      await client.connect("team08", "di34zlpjto");
+    } on Exception catch (e) {
+      print('client exception - $e');
+      client.disconnect();
+    }
+
+    // TODO: write payload
+
+    client.publishMessage("team08/prod/user/path", MqttQos.exactlyOnce, builder.payload);
+  }
+
+  void onConnected(){
+    print("AGENT - Connected to MQTT Broker");
+  }
 }
